3684708a7fafc32d83aa2973d9b99be859f877d4
Martin Savko <martin.savko@gmail.com>
2018-06-18 11:41:09 +0200

    Qt4_VimbaVideo:  -- Modifications to make the HardwareObject work with prosilica     gc1350c camera  -- Make pixel format configurable -- this could be developed further     to support bigger subset of formats supported by the device  -- vimba.startup() seems to be important for "GeVDiscoveryAllOnce" command     to work properly.  -- execution of "AcquistionStart" seems to necessary in general case to     launch the the image stream  -- add the test() function  -- cameraAccessMode=2 does not seem to be necessary. Should we turn it     into cofigurable option?

--- HardwareObjects/Qt4_VimbaVideo.py
+++ HardwareObjects/Qt4_VimbaVideo.py
@@ -1,11 +1,12 @@
-import time
 import gevent
 import atexit
 from pymba import *
 from PyQt4.QtGui import QImage, QPixmap
 
 from GenericVideoDevice import GenericVideoDevice
-
+import logging
+import cv2
+import numpy as np
 
 class Qt4_VimbaVideo(GenericVideoDevice):
 
@@ -19,34 +20,45 @@ class Qt4_VimbaVideo(GenericVideoDevice):
     def init(self):
         # start Vimba
         self.camera_id = u"%s" % self.getProperty("camera_id")
-        atexit.register(self.close_camera)
-        GenericVideoDevice.init(self)
+        self.poll_interval = self.getProperty("poll_interval")
+        self.pixel_format = self.getProperty("pixel_format", "RGB8Packed")
         self.image_dimensions = [1360, 1024]
+        self.log = logging.getLogger('HWR')
+        atexit.register(self.close_camera)
+        
+        GenericVideoDevice.init(self)
  
     def do_image_polling(self, sleep_time):
         with Vimba() as vimba:
             system = vimba.getSystem()
-   
+            vimba.startup() 
             if system.GeVTLIsPresent:
                 system.runFeatureCommand("GeVDiscoveryAllOnce")
-                time.sleep(0.2)
+                gevent.sleep(2)
             cameraIds = vimba.getCameraIds()
  
-            #self.camera = vimba.getCamera(self.camera_id)
-            self.camera = vimba.getCamera(cameraIds[-1])
-            self.camera.openCamera(cameraAccessMode=2)
-            self.camera.framerate = 1
+            self.camera = vimba.getCamera(self.camera_id)
+            self.camera.openCamera()
+            self.camera.PixelFormat = self.pixel_format
             self.frame0 = self.camera.getFrame()    # creates a frame
             self.frame0.announceFrame()
+            log.info('camera.startCapture()')
             self.camera.startCapture()
  
             self.image_dimensions = (self.frame0.width,
                                      self.frame0.height)
-
+            
+            self.camera.runFeatureCommand("AcquisitionStart")
             while True:
-                self.frame0.waitFrameCapture(1000)
-                self.frame0.queueFrameCapture()
-                self.qimage = QImage(self.frame0.getBufferByteData(),
+                self.frame0.waitFrameCapture(self.poll_interval)
+                try:
+                    self.frame0.queueFrameCapture()
+                except:
+                    log.info('frame dropped')
+                    continue
+                data = self.frame0.getBufferByteData()
+                img = np.ndarray(buffer=data, dtype=np.int8, shape=(self.frame0.height, self.frame0.width, self.frame0.pixel_bytes))
+                self.qimage = QImage(img,
                                      self.image_dimensions[0],
                                      self.image_dimensions[1],
                                      QImage.Format_RGB888)
@@ -55,7 +67,7 @@ class Qt4_VimbaVideo(GenericVideoDevice):
                                                        self.cam_mirror[1])
 
                 self.emit("imageReceived", QPixmap(self.qimage))
-                time.sleep(sleep_time)
+                gevent.sleep(sleep_time)
 
     def get_new_image(self):
         return self.qimage
@@ -75,3 +87,22 @@ class Qt4_VimbaVideo(GenericVideoDevice):
 
     def change_owner(self):
         pass
+
+def test():
+    import numpy as np
+    import time
+ 
+    with Vimba() as vimba:
+        vimba.startup()
+        system = vimba.getSystem()
+        system.runFeatureCommand("GeVDiscoveryAllOnce")
+        gevent.sleep(2)
+        cameraIds = vimba.getCameraIds()
+        print 'cameraIds', cameraIds
+        camera = vimba.getCamera(cameraIds[0])
+        camera.openCamera()
+        print 'acquistionMode', camera.AcquisitionMode
+             
+
+if __name__ == '__main__':
+    test()
