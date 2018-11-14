0a66ea4caffbef22027f3c0e64aae6e9cf6f208c
Martin Savko <savko@synchrotron-soleil.fr>
2018-11-14 19:16:04 +0100

    SOLEIL PX2Resolution.py   -- camelCase -> snake for limits and state   -- handling "stateChanged" and "positionChanged" signals from dtox   -- handling "valueChanged" signal from energy hardware object

--- HardwareObjects/SOLEIL/PX2/PX2Resolution.py
+++ HardwareObjects/SOLEIL/PX2/PX2Resolution.py
@@ -4,16 +4,22 @@ from resolution import resolution
 from beam_center import beam_center
 
 class PX2Resolution(Resolution):
-
+    OFF, READY, MOVING = 1, 2, 4
+    TANGO_TO_MOTOR_STATE = {"ALARM": OFF,
+                            "STANDBY": READY,
+                            "MOVING": MOVING}
+    DEFAULT_RESOLUTION = 2.
+    
     def __init__(self, *args, **kwargs):
         Resolution.__init__(self, *args, **kwargs)
 
+    
     def init(self):
         
         self.resolution_motor = resolution()
         self.beam_center = beam_center()
         
-        self.currentResolution = None
+        self.currentResolution = self.get_resolution() #self.DEFAULT_RESOLUTION
         
         self.energy_channel = self.getChannelObject("energy")
         self.energy_channel.connectSignal("update", self.update_resolution)
@@ -22,8 +28,7 @@ class PX2Resolution(Resolution):
         self.energy_state_channel.connectSignal("update", self.update_energy_state)
         
         self.detector_distance_channel = self.getChannelObject("detector_position")
-        self.energy_channel.connectSignal("update", self.update_resolution)
-        self.energy_channel.connectSignal("valueChanged", self.update_resolution)
+        #self.energy_channel.connectSignal("valueChanged", self.update_resolution)
         self.detector_distance_channel.connectSignal("update", self.update_resolution)
         
         self.detector_position_state_channel = self.getChannelObject("detector_position_state")
@@ -35,46 +40,78 @@ class PX2Resolution(Resolution):
         self.det_radius = self.getProperty("detector_radius")
         self.det_width = self.getProperty("detector_width")
         self.det_height = self.getProperty("detector_height")
+        
+        self.connect(self.dtox, "stateChanged", self.dtoxStateChanged)
+        self.connect(self.dtox, "positionChanged", self.dtoxPositionChanged) 
+        self.connect(self.energy, "valueChanged", self.energyChanged)
     
-    def get_limits(self):
-        return self.getLimits()
     
     def connectNotify(self, signal):
         if signal == "stateChanged":
-            self.dtoxStateChanged(self.getState())
+            self.dtoxStateChanged(self.get_state())
+    
     
     def move(self, resolution):
         self.resolution_motor.set_resolution(resolution)
     
-    def getState(self):
-        return self.detector_position_state_channel.getValue()
+    
+    def get_state(self):
+        state = self.detector_position_state_channel.getValue()
+        if state != None and str(state) in self.TANGO_TO_MOTOR_STATE:
+            state = self.TANGO_TO_MOTOR_STATE[state.name]
+        else:
+            state = self.OFF
+        return state
+    
     
     def get_beam_centre(self, dtox=None):
         return self.beam_center.get_beam_center()
     
-    def getLimits(self):
+    
+    def get_limits(self):
         return self.resolution_motor.get_resolution_limits()
         
+    
     def dtoxStateChanged(self, state=None):
         self.update_detector_position_state(state)
         
+    
     def update_detector_position_state(self, state=None):
+        if state != None and str(state) in self.TANGO_TO_MOTOR_STATE:
+            state = self.TANGO_TO_MOTOR_STATE[state.name]
+        elif type(state) == int:
+            pass
+        else:
+            state = self.OFF
         self.emit("stateChanged", state)
         
+    
     def update_energy_state(self, state=None):
+        if state != None and str(state) in self.TANGO_TO_MOTOR_STATE:
+            state = self.TANGO_TO_MOTOR_STATE[state.name]
+        elif type(state) == int:
+            pass
+        else:
+            state = self.OFF
         self.emit("stateChanged", state)
         
+    
     def update_resolution(self, values=None):
-        self.currentResolution = self.resolution_motor.get_resolution()
+        self.currentResolution = self.get_resolution()
         self.emit("positionChanged", self.currentResolution)
         self.emit("valueChanged", self.currentResolution)
-        self.emit("statechanged", self.getState())
+        
     
     def stop(self):
         self.resolution_motor.stop()
         
+    
     def get_detector_distance(self):
         return self.dtox.get_position()
     
+    
     def get_resolution(self):
-        return self.resolution_motor.get_resolution()
+        resolution = self.resolution_motor.get_resolution()
+        return resolution
+    
+    
