3814d57891a471745db355afa8bd788e2b752945
Martin Savko <savko@synchrotron-soleil.fr>
2021-04-21 16:53:48 +0200

    This seems a better solution -- rely on test done before.

--- mxcubecore/HardwareObjects/abstract/AbstractVideoDevice.py
+++ mxcubecore/HardwareObjects/abstract/AbstractVideoDevice.py
@@ -51,10 +51,7 @@ module_names = ["qt", "PyQt5", "PyQt4"]
 
 if any(mod in sys.modules for mod in module_names):
     USEQT = True
-    try:
-        from PyQt5.QtGui import QImage, QPixmap
-    except ImportError:
-        from PyQt4.QtGui import QImage, QPixmap
+    from mxcubecore.utils.qt_import import QPixmap, QImage
 else:
     USEQT = False
     from PIL import Image
